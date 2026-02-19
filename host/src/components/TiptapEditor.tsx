7ecee268eec96ba9fa8673933cb48529e1cbd3d8
Rohan Chaudhari <rohan1chaudhari@gmail.com>
2026-02-19 20:13:21 +0100

    fix: add immediatelyRender: false to TiptapEditor to prevent SSR hydration error

--- host/src/components/TiptapEditor.tsx
+++ host/src/components/TiptapEditor.tsx
@@ -24,6 +24,7 @@ export function TiptapEditor({ value, onChange, placeholder }: TiptapEditorProps
       }),
     ],
     content: value,
+    immediatelyRender: false,
     onUpdate: ({ editor }) => {
       onChange(editor.getText());
     },
